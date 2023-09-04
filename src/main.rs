6d5abea3d618891fae65b1c752b458f7e2b9e8fa
ONLYIWIN21 <63803448+ONLYIWIN21@users.noreply.github.com>
2023-09-04 14:40:05 -0400

    read file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io::Read;
+use std::fs::File;
+use std::env::args;
+
 fn main() {
-    println!("Hello, world!");
+    let path = args().nth(1).expect("No file provided"); 
+    let mut file = File::open(path).expect("Could not open file");
+    let mut text = String::new();
+    file.read_to_string(&mut text).expect("Could not read from file");
+    println!("{}", text);
 }
