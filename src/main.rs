550165a68bf6dd71e8045e25fc650290a88e15ff
ONLYIWIN21 <63803448+ONLYIWIN21@users.noreply.github.com>
2023-09-04 21:39:37 -0400

    added tokenizer

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@ use std::io::Read;
 use std::fs::File;
 use std::env::args;
 
+mod tokenizer;
+
 fn main() {
     let path = args().nth(1).expect("No file provided"); 
     let mut file = File::open(path).expect("Could not open file");
     let mut text = String::new();
     file.read_to_string(&mut text).expect("Could not read from file");
+    text.push('\n');
     println!("{}", text);
+    let mut tokenizer = tokenizer::Tokenizer::new(text);
+    tokenizer.tokenize();
 }
---
+++ src/tokenizer.rs
@@ -0,0 +1,101 @@
+pub struct Token {
+    pub t_type: TokenType,
+    pub val: Option<String>,
+}
+
+pub enum TokenType {
+    RET,
+    INT,
+    SEMI,
+}
+
+pub struct Tokenizer {
+    text: String,
+    pos: i32,
+    pub tokens: Vec<Token>,
+}
+
+impl Tokenizer {
+    pub fn new(text: String) -> Tokenizer {
+        return Tokenizer {
+            text,
+            pos: 0,
+            tokens: Vec::new(),
+        };
+    }
+
+    fn tokenize_word(&mut self) {
+        use TokenType::*;
+        let mut token = String::new();
+        loop {
+            match self.peek() {
+                Some(ch) => match ch {
+                    '0'..='9' | 'a'..='z' | 'A'..='Z' => {
+                        self.next();
+                        token.push(ch);
+                    }
+                    _ => break,
+                },
+                None => break,
+            }
+        }
+        match token.as_str() {
+            "return" => self.tokens.push(Token {
+                t_type: RET,
+                val: None,
+            }),
+            _ => panic!("Not a valid word"),
+        }
+    }
+
+    fn tokenize_num(&mut self) {
+        use TokenType::*;
+        let mut val = String::new();
+        loop {
+            match self.peek() {
+                Some(ch) => match ch {
+                    '0'..='9' => {
+                        self.next();
+                        val.push(ch);
+                    }
+                    _ => break,
+                },
+                None => break,
+            }
+        }
+        self.tokens.push(Token {
+            t_type: INT,
+            val: Some(val),
+        });
+    }
+
+    pub fn tokenize(&mut self) {
+        use TokenType::*;
+        loop {
+            match self.peek() {
+                Some(ch) => match ch {
+                    ';' => {
+                        self.tokens.push(Token {
+                            t_type: SEMI,
+                            val: None,
+                        });
+                        self.next();
+                    }
+                    ' ' | '\n' | '\r' => self.next(),
+                    'a'..='z' | 'A'..='Z' => self.tokenize_word(),
+                    '0'..='9' => self.tokenize_num(),
+                    _ => panic!("Not a valid token {:?}", ch),
+                },
+                None => break,
+            }
+        }
+    }
+
+    fn peek(&self) -> Option<char> {
+        return self.text.chars().nth(self.pos as usize);
+    }
+
+    fn next(&mut self) {
+        self.pos += 1;
+    }
+}
